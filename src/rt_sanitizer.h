d35ad254851aa2b1d9cf84995939462b59aced26
dimbombay <dimkuzalex1999@gmail.com>
2026-06-15 13:47:45 +0300

    rtsan macro fix

--- src/rt_sanitizer.h
+++ src/rt_sanitizer.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#if defined(__has_feature) && __has_feature(realtime_sanitizer)
-    #define RT_SAN [[clang::nonblocking]]
+#if defined(__has_feature)
+    #if __has_feature(realtime_sanitizer)
+        #define RT_SAN [[clang::nonblocking]]
+    #else
+        #define RT_SAN
+    #endif
 #else
     #define RT_SAN
 #endif
